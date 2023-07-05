57b41b17d9a1fd59ec1390d2ebce495c5d783902
Florenz <florenzthedev@gmail.com>
2023-07-05 12:26:57 -0700

    Initial commit

---
+++ common/butterfly-dit.pl
@@ -0,0 +1,47 @@
+use strict;
+use warnings;
+
+sub log2 {
+    my $n = shift;
+    return log($n)/log(2);
+}
+
+use integer;
+my ($length) = @ARGV;
+my $width = log2($length);
+
+my $os = 2; #offset
+my $ml = 2; #multiplier
+
+my @brp = ();
+for (my $i = 0; $i < $length; $i++) {
+  my $r = 0;
+  for (my $j = 0; $j < $width; $j++) {
+     $r = $r | (1 << (($width - 1) - $j)) if ($i & (1 << $j));
+  }
+  $brp[$i] = $r;
+}
+
+my $full_width = $width * ($os + $ml);
+
+for(0..($length - 1)){
+  print "\\draw[thick] (-1,-$_) -- ($full_width,-$_);\n";
+  print "\\addplot[mark=*] coordinates {(-1,-$_)} node [left] {\$x_$_\$};\n";
+  my $bigX = $brp[$_];
+  print "\\addplot[mark=*] coordinates {($full_width,-$_)} node [right] {\$X_$bigX\$};\n";
+}
+
+for(my $j = 0; $j < $width; $j++){
+  my $block = $length / ($j + 1);
+  for(my $g = 0; $g < $length; $g += $block){
+    for(my $k = 0; $k < $block / 2; $k++){
+      my $lo = $g + $k;
+      my $hi = $lo + ($block / 2);
+      my $st = $j * ($ml + $os);
+      my $nx = $st + $ml;
+      my $kn = -$k; #so we don't get a negative zero
+      print "\\draw[-Stealth,thick,line_color] ($st,-$lo) -- ($nx,-$hi) node[above right] {\$x_$lo-\\omega_$block^{$kn}\\cdot x_$hi\$};\n";   
+      print "\\draw[-Stealth,thick,mark_color] ($st,-$hi) -- ($nx,-$lo) node[above right] {\$x_$lo+\\omega_$block^{$kn}\\cdot x_$hi\$};\n";   
+    }
+  }
+}
---
+++ common/fft.pl
@@ -0,0 +1,58 @@
+use strict;
+use warnings;
+use autodie;
+use Regexp::Common;
+use Math::Complex;
+
+sub FFT {
+  my $x = shift;
+  my $n = scalar(@$x);
+  return if($n < 2);
+
+  my @xe = ();
+  for(my $j = 0; $j < $n; $j += 2){
+    push @xe, $$x[$j];
+  }
+  FFT(\@xe);
+
+  my @xo = ();
+  for(my $j = 1; $j < $n; $j += 2){
+    push @xo, $$x[$j];
+  }
+
+  FFT(\@xo);
+
+  for(my $j = 0; $j < ($n / 2); $j++){
+    my $product = $xo[$j] * exp((i * 2 * pi * $j) / $n);
+    $$x[$j] = $xe[$j] + $product;
+    $$x[$j + ($n / 2)] = $xe[$j] - $product;
+  }
+}
+
+my ($filename, $seconds) = @ARGV;
+open(TABLE, '<', $filename);
+
+my @y = ();
+
+while(<TABLE>){
+  my $letter = substr( $_, 0, 1 );
+  next if($letter eq "#" or $letter eq "\n");
+  my @pair = $_ =~ /($RE{num}{real})/g;
+  push @y, $pair[1];
+}
+
+my $n = scalar(@y);
+(($n & ($n - 1)) == 0) or die("Input not a power of two in size!");
+my $hertz = $n / $seconds; 
+
+
+
+close(TABLE);
+
+FFT(\@y);
+
+open(TABLE, '>', $filename);
+
+for(my $j = 0; $j < $n; $j++){
+  print TABLE ($j * ($hertz/$n)), " ", abs($y[$j])/$n, "\n";
+}
